f85b8ca0dad038660166b576cab2061d4f966aa8
Michael Toppa <public@toppa.com>
2011-09-24 18:40:27 -0400

    updated versio number

--- start.php
+++ start.php
@@ -4,7 +4,7 @@ Plugin Name: Toppa Plugins Libraries for WordPress
 Plugin URI: http://www.toppa.com/toppa-plugin-libraries-for-wordpress/
 Description: Libraries to facilitate the use of Agile coding techniques for developing WordPress plugins. Contains required libraries for using plugins from toppa.com
 Author: Michael Toppa
-Version: 1.0.2
+Version: 1.0.3
 Author URI: http://www.toppa.com
 */
 
