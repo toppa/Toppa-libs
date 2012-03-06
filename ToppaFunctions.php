f787f206b9869de3022c48dd186236d2e9b50162
Michael Toppa <public@toppa.com>
2012-03-05 21:29:10 -0500

    Added followRedirect()  and arrayMergeRecursiveForSettings()

--- ToppaFunctions.php
+++ ToppaFunctions.php
@@ -87,4 +87,55 @@ class ToppaFunctions {
 
         return $fileNameParts[$lastIndexPosition];
     }
+
+    // thank you http://www.webmasterworld.com/php/3681920.htm
+    public static function followRedirect($url) {
+        $redirect_url = null;
+
+        if (function_exists("curl_init")) {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_NOBODY, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            curl_close($ch);
+        }
+        else {
+            $url_parts = parse_url($url);
+            $sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80));
+            $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n";
+            $request .= 'Host: ' . $url_parts['host'] . "\r\n";
+            $request .= "Connection: Close\r\n\r\n";
+            fwrite($sock, $request);
+            $response = fread($sock, 2048);
+            fclose($sock);
+        }
+
+        $header = "Location: ";
+        $pos = strpos($response, $header);
+        if ($pos === false) {
+            return false;
+        }
+        else {
+            $pos += strlen($header);
+            $redirect_url = substr($response, $pos, strpos($response, "\r\n", $pos)-$pos);
+            return $redirect_url;
+        }
+    }
+
+    // thank you http://us.php.net/manual/en/function.array-merge-recursive.php#102379
+    // any subarrays must exist in $oldArray
+    public static function arrayMergeRecursiveForSettings($oldArray, $newArray) {
+        foreach ($newArray as $key => $value) {
+            if (array_key_exists($key, $oldArray) && is_array($value)) {
+                $oldArray[$key] = ToppaFunctions::arrayMergeRecursiveForSettings($oldArray[$key], $newArray[$key]);
+            }
+
+            else {
+                $oldArray[$key] = $value;
+            }
+        }
+
+        return $oldArray;
+    }
 }
